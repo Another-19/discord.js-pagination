a75bb35bd00f994747401c5a9324df106ee7e3e2
Another7139 <15141869+Another7139@users.noreply.github.com>
2021-04-03 23:24:02 +0800

    testing

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout =
 	if (!pages) throw new Error('Pages are not given.');
 	if (emojiList.length !== 2) throw new Error('Need two emojis.');
 	let page = 0;
-	const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+	const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1}/${pages.length}`));
 	for (const emoji of emojiList) await curPage.react(emoji);
 	const reactionCollector = curPage.createReactionCollector(
 		(reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
 		{ time: timeout }
 	);
 	reactionCollector.on('collect', reaction => {
-		reaction.users.remove(msg.author);
 		switch (reaction.emoji.name) {
 			case emojiList[0]:
 				page = page > 0 ? --page : pages.length - 1;
@@ -21,7 +20,20 @@ const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout =
 			default:
 				break;
 		}
-		curPage.edit(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length}`));
+	});
+	reactionCollector.on('remove', reaction => {
+		switch (reaction.emoji.name) {
+			case emojiList[0]:
+				page = page > 0 ? --page : pages.length - 1;
+				break;
+			case emojiList[1]:
+				page = page + 1 < pages.length ? ++page : 0;
+				break;
+			default:
+				break;
+		}
+		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length}`));
 	});
 	reactionCollector.on('end', () => {
 		if (!curPage.deleted) {
