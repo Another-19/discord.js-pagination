8da0b4b3eb4d8dc4592b46e57dc8a1d00f5049c5
Another7139 <15141869+Another7139@users.noreply.github.com>
2021-04-08 23:07:45 +0800

    rework, should fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", "➡️", "⏭️", "❎", "829731154487803944"], timeout = 300000) => {
+const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", "➡️", "⏭️", "829731154487803944"], timeout = 300000) => {
 	if (!message && !message.channel) throw new Error('Channel is inaccessible.');
 	if (!pages) throw new Error('Pages are not given.');
 	// if (emojiList.length !== 2) throw new Error('Need two emojis.');
@@ -9,7 +9,7 @@ const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", 
 		(reaction, user) => emojiList.includes(reaction.emoji.id) && !user.bot,
 		{time: timeout, dispose: true});
 	reactionCollector.on('collect', reaction => {
-		switch (reaction.emoji.id) {
+		switch (reaction.emoji.name) {
 			case emojiList[0]:
 				page = page + 1;
 				break;
@@ -22,17 +22,15 @@ const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", 
 			case emojiList[3]:
 				page = pages.length - 1;
 				break;
-			case "829731154487803944":
-				curPage.reactions.removeAll();
-				break;
 			default:
+				curPage.reactions.removeAll();
 				break;
 		}
 		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
 	});
 	
 	reactionCollector.on('remove', reaction => {
-		switch (reaction.emoji.id) {
+		switch (reaction.emoji.name) {
 			case emojiList[0]:
 				page = page + 1;
 				break;
@@ -45,10 +43,8 @@ const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", 
 			case emojiList[3]:
 				page = pages.length - 1;
 				break;
-			case "829731154487803944":
-				curPage.reactions.removeAll();
-				break;
 			default:
+				curPage.reactions.removeAll();
 				break;
 		}
 		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
