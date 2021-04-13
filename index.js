ebd60d6bd72cb579e7e066946a045506dcfaffaf
Another7139 <15141869+Another7139@users.noreply.github.com>
2021-04-13 18:35:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,45 +6,47 @@ const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", 
 	const reactionCollector = curPage.createReactionCollector((reaction, user) => emojiList.includes(reaction.emoji.id || reaction.emoji.name) && !user.bot, {time: timeout, dispose: true});
 
 	reactionCollector.on("collect", (reaction, user) => {
-		console.log(user)
-		console.log("\n\n" + player)
-		switch (reaction.emoji.name && user.id == player) {
-			case emojiList[0]:
-				page = 0;
-				break;
-			case emojiList[1]:
-				page = page > 0 ? --page : pages.length - 1;
-				break;
-			case emojiList[2]:
-				page = page + 1 < pages.length ? ++page : 0;
-				break;
-			case emojiList[3]:
-				page = pages.length - 1;
-				break;
-			default:
-				curPage.reactions.removeAll();
-				break;
+		if (user.id == message.author.id) {
+			switch (reaction.emoji.name) {
+				case emojiList[0]:
+					page = 0;
+					break;
+				case emojiList[1]:
+					page = page > 0 ? --page : pages.length - 1;
+					break;
+				case emojiList[2]:
+					page = page + 1 < pages.length ? ++page : 0;
+					break;
+				case emojiList[3]:
+					page = pages.length - 1;
+					break;
+				default:
+					curPage.reactions.removeAll();
+					break;
+			}
 		}
 		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
 	});
 	
 	reactionCollector.on("remove", (reaction, user) => {
-		switch (reaction.emoji.name && user.id == player) {
-			case emojiList[0]:
-				page = 0;
-				break;
-			case emojiList[1]:
-				page = page > 0 ? --page : pages.length - 1;
-				break;
-			case emojiList[2]:
-				page = page + 1 < pages.length ? ++page : 0;
-				break;
-			case emojiList[3]:
-				page = pages.length - 1;
-				break;
-			default:
-				curPage.reactions.removeAll();
-				break;
+		if (user.id == message.author.id) {
+			switch (reaction.emoji.name) {
+				case emojiList[0]:
+					page = 0;
+					break;
+				case emojiList[1]:
+					page = page > 0 ? --page : pages.length - 1;
+					break;
+				case emojiList[2]:
+					page = page + 1 < pages.length ? ++page : 0;
+					break;
+				case emojiList[3]:
+					page = pages.length - 1;
+					break;
+				default:
+					curPage.reactions.removeAll();
+					break;
+			}
 		}
 		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
 	});
