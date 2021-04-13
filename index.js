529a9c68bd4c502748c6ed57e59aca41e04644e8
Another7139 <15141869+Another7139@users.noreply.github.com>
2021-04-13 18:22:14 +0800

    remove await, testing sync

--- index.js
+++ index.js
@@ -1,61 +1,53 @@
-const paginationEmbed = async (message, pages, emojiList = ["⏮️", "⬅️", "➡️", "⏭️", "829731154487803944"], timeout = 300000) => {
-	if (!message && !message.channel) throw new Error('Channel is inaccessible.');
-	if (!pages) throw new Error('Pages are not given.');
-	// if (emojiList.length !== 2) throw new Error('Need two emojis.');
+const paginationEmbed = (message, pages, emojiList = ["⏮️", "⬅️", "➡️", "⏭️", "829731154487803944"], timeout = 300000) => { // async
 	let page = 0;
 	let player = message.author.id;
-	const curPage = await message.channel.send(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
-	for (const emoji of emojiList) await curPage.react(emoji);
-	const reactionCollector = curPage.createReactionCollector(
-		(reaction, user) => emojiList.includes(reaction.emoji.id || reaction.emoji.name) && !user.bot,
-		{time: timeout, dispose: true});
-	reactionCollector.on('collect', reaction, user => {
-		if (user.id == player) {
-			switch (reaction.emoji.name) {
-				case emojiList[0]:
-					page = 0;
-					break;
-				case emojiList[1]:
-					page = page > 0 ? --page : pages.length - 1;
-					break;
-				case emojiList[2]:
-					page = page + 1 < pages.length ? ++page : 0;
-					break;
-				case emojiList[3]:
-					page = pages.length - 1;
-					break;
-				default:
-					curPage.reactions.removeAll();
-					break;
-			}
+	const curPage = message.channel.send(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`)); // await
+	for (const emoji of emojiList) curPage.react(emoji); // await
+	const reactionCollector = curPage.createReactionCollector((reaction, user) => emojiList.includes(reaction.emoji.id || reaction.emoji.name) && !user.bot, {time: timeout, dispose: true});
+
+	reactionCollector.on("collect", (reaction, user) => {
+		switch (reaction.emoji.name && user.id == player) {
+			case emojiList[0]:
+				page = 0;
+				break;
+			case emojiList[1]:
+				page = page > 0 ? --page : pages.length - 1;
+				break;
+			case emojiList[2]:
+				page = page + 1 < pages.length ? ++page : 0;
+				break;
+			case emojiList[3]:
+				page = pages.length - 1;
+				break;
+			default:
+				curPage.reactions.removeAll();
+				break;
 		}
 		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
 	});
 	
-	reactionCollector.on('remove', reaction, user => {
-		if (user.id == player) {
-			switch (reaction.emoji.name) {
-				case emojiList[0]:
-					page = 0;
-					break;
-				case emojiList[1]:
-					page = page > 0 ? --page : pages.length - 1;
-					break;
-				case emojiList[2]:
-					page = page + 1 < pages.length ? ++page : 0;
-					break;
-				case emojiList[3]:
-					page = pages.length - 1;
-					break;
-				default:
-					curPage.reactions.removeAll();
-					break;
-			}
+	reactionCollector.on("remove", (reaction, user) => {
+		switch (reaction.emoji.name && user.id == player) {
+			case emojiList[0]:
+				page = 0;
+				break;
+			case emojiList[1]:
+				page = page > 0 ? --page : pages.length - 1;
+				break;
+			case emojiList[2]:
+				page = page + 1 < pages.length ? ++page : 0;
+				break;
+			case emojiList[3]:
+				page = pages.length - 1;
+				break;
+			default:
+				curPage.reactions.removeAll();
+				break;
 		}
 		curPage.edit(pages[page].setFooter(`Page ${page + 1}/${pages.length} - Use the reactions below to change the embed pages.`));
 	});
 	
-	reactionCollector.on('end', () => {
+	reactionCollector.on("end", () => {
 		if (!curPage.deleted) {
 			curPage.reactions.removeAll()
 		}
